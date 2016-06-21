82cf68e2cf9260300f81b62c428635ca1af185e2
David Loughnane <david.loughnane1@gmail.com>
2016-06-21 10:01:37 +0100

    structural network construction

---
+++ build_structural_net.py
@@ -0,0 +1,17 @@
+import scipy.io 
+
+
+#### LOAD DESTRIEUX PARCELLATION MASK ####
+parcels_source_left = scipy.io.loadmat('100307_aparc_a2009s_L.mat')
+#parcels_source_right = scipy.io.loadmat('100307_aparc_a2009s_R.mat')
+
+parcels = load_parcels_left['aparc'] #+ load_parcels_right['aparc']
+
+
+
+
+#### LOAD FUNCTIONAL TIME SERIES ####
+functional_ts_source_left = scipy.io.loadmat('100307_dt_series_fix_normalized_connected_L.mat')
+#functional_ts_source_right = scipy.io.loadmat('100307_dt_series_fix_normalized_connected_R.mat')
+
+functional_ts = functional_ts_source_left['dtseries'] #+ functional_ts_source_right['dtseries']
