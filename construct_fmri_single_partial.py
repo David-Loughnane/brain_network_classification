423678a6933aab3e8c8b4ba8be673bb1baf5e19a
David Loughnane <dl4415@ray20.doc.ic.ac.uk>
2016-08-15 12:10:57 +0100

    glasser parcellation partial correlation

--- construct_fmri_single_partial.py
+++ construct_fmri_single_partial.py
@@ -7,14 +7,14 @@ import csv
 
 VOXEL_TOTAL_L = 29696
 VOXEL_TOTAL_R = 29716
-HEMIS_PARCELS = 76
+HEMIS_PARCELS = 100
 BRAIN_PARCELS = HEMIS_PARCELS*2
 TS_LENGTH = 2400
-NUM_SUBJECTS = 100
+NUM_SUBJECTS = 99
 
 
 FILE_PATH = "/vol/vipdata/data/HCP100/"
-
+FILE_PATH_MAPPING = "/vol/medic02/users/sparisot/ClionProjects/openGm_maxflow/build/"
 
 
 subjectIDs = []
@@ -27,8 +27,20 @@ feature_vector = np.zeros((NUM_SUBJECTS,(BRAIN_PARCELS*(BRAIN_PARCELS-1)/2)), dt
 
 def functional_parcellation_mapping(subject, hemisphere):
 	#### DESTRIEUX PARCELLATION MAPPING ####
+	'''
 	parcels_source = scipy.io.loadmat(FILE_PATH + "{0}/processed/{0}_aparc_a2009s_{1}.mat".format(subjectIDs[subject], hemisphere))
 	parcels = np.array(parcels_source['aparc'])
+	print parcels
+   	'''
+
+	#### GRAMPA PARCELLATION MAPPING ####
+	parcel_list = []
+	with open(FILE_PATH_MAPPING + "{0}/{1}/fmri_{0}_100_initR10.txt".format(subjectIDs[subject], hemisphere)) as f:
+		for line in f:
+			for word in line.split():
+				parcel_list.append([round(float(word))])
+	parcels = np.array(parcel_list)
+	parcels = parcels.astype(int)
 
 	# number of voxels assigned to each parcel to calculate average
 	parcel_count_list = []
@@ -64,28 +76,27 @@ def functional_parcellation_mapping(subject, hemisphere):
 
 
 connectome_input = []
-
 for subject in range(len(subjectIDs)):
-	left_parcellated_ts = functional_parcellation_mapping(subject, "L")
-        right_parcellated_ts = functional_parcellation_mapping(subject, "R")
-        whole_brain_ts = np.concatenate((left_parcellated_ts, right_parcellated_ts), axis=0) 
-
-        connectome_input.append(whole_brain_ts)
-
+	print subjectIDs[subject]
+	if subjectIDs[subject] == '397760':	
+		continue
+	else:
+		left_parcellated_ts = functional_parcellation_mapping(subject, "L")
+		right_parcellated_ts = functional_parcellation_mapping(subject, "R")
+		whole_brain_ts = np.concatenate((left_parcellated_ts, right_parcellated_ts), axis=0)
+		connectome_input.append(whole_brain_ts)
 
 # functional correlation matrix
 conn_measure = nilearn.connectome.ConnectivityMeasure(kind='partial correlation')
 func_corr = conn_measure.fit_transform(connectome_input)
 
-print 'Connectome computation complete'
 
-'''
+
+print 'Connectome computation complete'
 print 'connectome'
 print func_corr.shape
-'''
 
-for subject in range(len(subjectIDs)):
-	# extract upper right hand corner of correlation matrix
+for subject in range(NUM_SUBJECTS):
 	for i in range((BRAIN_PARCELS*(BRAIN_PARCELS-1)/2)):
 		for j in range(BRAIN_PARCELS):
 			for k in range(j+1, BRAIN_PARCELS):
@@ -108,8 +119,8 @@ for subject in range(len(subjectIDs)):
 	print(func_corr[subject])
 	print('\n')
 
+
 print('FUNCTIONAL CORRELATION FEATURE VECTOR')
 print(type(feature_vector))
 print(feature_vector.shape)
-np.savetxt('networks/fmri_dest_partial.txt', feature_vector)
-
+np.savetxt('networks/fmri_grampa_single100_partial_R10.txt', feature_vector)
