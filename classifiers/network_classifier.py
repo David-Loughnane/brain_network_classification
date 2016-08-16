16c3a211b1d98e5a91a65d7191845052ea6591ff
David Loughnane <dl4415@ray20.doc.ic.ac.uk>
2016-08-16 11:58:16 +0100

    classifer file loop

--- classifiers/network_classifier.py
+++ classifiers/network_classifier.py
@@ -5,7 +5,8 @@ import os
 import math
 import numpy as np
 from sklearn.ensemble import RandomForestClassifier
-#from sklearn.ensemble import AdaBoostClassifier
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn import tree
 from sklearn import cross_validation
 from sklearn import metrics
 
@@ -15,7 +16,6 @@ NETWORKS_PATH = '/homes/dl4415/Documents/project/brain_network_classification/ne
 
 def cv_classifier(input_features):
 	loaded_features = np.loadtxt(NETWORKS_PATH + input_features)
-	print("Size of feature vector: ", loaded_features.shape)
 
 	subject_attribs = np.genfromtxt('../class_labels.csv', delimiter=',', dtype = str, skip_header = 1)
 	labels = []
@@ -29,8 +29,8 @@ def cv_classifier(input_features):
 	#print("Memory usage before: {}MB".format(memory_usage()))
 
 	#### INSTANTIATE CLASSIFIER ####
-	clf = RandomForestClassifier(n_estimators=100)
-	#clf = AdaBoostClassifier(n_estimators=100, base_estimator=tree.DecisionTreeClassifier(criterion="entropy", min_samples_split=100, max_depth=20))
+	#clf = RandomForestClassifier(n_estimators=100)
+	clf = AdaBoostClassifier(n_estimators=100)
 
 	#### CROSS VALIDATION ####
 	#t0 = time()
@@ -38,6 +38,7 @@ def cv_classifier(input_features):
 	#t1 = time()
 
 	print("Parcellation: ", input_features)
+	print("Size of feature vector: ", loaded_features.shape)
 	print("Scores: ", scores)
 	print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
 
@@ -45,11 +46,9 @@ def cv_classifier(input_features):
 	#print("Memory usage after: {}MB".format(memory_usage()))
 
 
-i = 0
 for roots, dirs, files in os.walk(NETWORKS_PATH):
 	if roots == NETWORKS_PATH:
 		for file in files:
-			print 'CLASSIFIER #', i
 			cv_classifier(file)
 			print '\n'
 			i =+ 1
